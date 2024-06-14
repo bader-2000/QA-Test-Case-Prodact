59de9307253554632087542e917745682f0aa989
Bader Abu sa'aleek <bader.sleman2000@gmail.com>
2024-06-14 19:39:43 +0300

    modifay code

--- src/testCaseProdact/LogInTest.java
+++ src/testCaseProdact/LogInTest.java
@@ -1,5 +1,6 @@
 package testCaseProdact;
 import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,15 +15,15 @@ public class LogInTest {
 	String password = "secret_sauce"; 
 	String website = "https://www.saucedemo.com/"; 
 	String login = "//input[@type='submit']";
-	String expectedName = "Products";
-	String actualName ;
-	String expactedPrice = "$7.99" ; 
+	boolean expectedName = true ;
+	boolean actualName ;
+	boolean expactedPrice = true ; 
 	String actualPrice;
 	
 	@Test (priority = 1 )
 	public void setUpTest () { 
 		driver.get(website);		
-	
+	    System.out.println(website.contains("en"));
 	}
 
 	@Test (priority = 2 )
@@ -36,9 +37,8 @@ public class LogInTest {
 
 	@Test (priority = 3 )
 	public void VireficationTheTextPradact() { 
-		     actualName = driver.findElement(By.xpath("//span[@class='title']")).getText();
-			 System.out.println(actualName);				
-			 Assert.assertEquals(actualName, expectedName);
+		     WebElement theMianNameOnTheWebSite = driver.findElement(By.xpath("//span[@class='title']"));	      			
+			 Assert.assertEquals(expectedName,theMianNameOnTheWebSite.isDisplayed());
 	}
 	
 	@Test (priority = 4 )
@@ -47,18 +47,18 @@ public class LogInTest {
 		WebElement selectOption = driver.findElement(By.xpath("//select[@data-test='product-sort-container']"));
 		Select myselect = new Select(selectOption);
 		myselect.selectByIndex(2);	
-		
-		
-			
-	        	
+        	
 	} 
 	
 	@Test (priority = 5)
 	public void VerificationiLowPrice () throws InterruptedException { 
 		        Thread.sleep(1000);		 
-			    List<WebElement> listPrices = driver.findElements(By.xpath("//div[@class='inventory_item_price']"));	
-				actualPrice = listPrices.get(0).getText();			
-				Assert.assertEquals(expactedPrice,actualPrice);
+			    List<WebElement> listPrices = driver.findElements(By.xpath("//div[@class='inventory_item_price']"));
+			    String theLowestPriceAsTest = listPrices.get(0).getText().replace("$", "");
+			    String theHighestPriceAsTest   = listPrices.get(listPrices.size()-1).getText().replace("$", "");
+			    double lowestPrice = Double.parseDouble(theLowestPriceAsTest );
+			    double highestPrice = Double.parseDouble(theHighestPriceAsTest );		
+				Assert.assertEquals(lowestPrice < highestPrice , expactedPrice ,"the price first item is not low every all items ");
 	        	
 }
 	
